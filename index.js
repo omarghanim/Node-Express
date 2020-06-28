56c80aa601d758bba83a9b72203fd7c2515831e1
omar ghanim <omarmghanim@gmail.com>
2020-06-28 11:36:58 +0200

    Node=Express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+
+const app =express();
+const hostname="localhost";
+const port = 3000;
+app.use(function(req,res){                        //app.use((req,res)=>{the next code})
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type","text/html");
+  res.end("<h1>This is Express Server</h1>");
+
+})
+app.listen(3000,function(req,res){
+console.log("server running at https://"+hostname+":"+port);
+})
+
+
+// we can use this
+// const server = http.createServer(app);
+// server.listen(port,hostname,()=>{
+// console.log("server running at https://"+hostname+":"+port);
+// })
